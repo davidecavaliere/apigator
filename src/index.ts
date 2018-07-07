571381092aace21bc162da7499a7d4c0b12e7858
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-07-07 15:18:59 +0200

    updating deps, linting and exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-export * from './lib/async';
-export * from './lib/hash';
-export * from './lib/number';
+export * from './lib/services/service.decorator';
+export * from './lib/endpoint/endpoint.decorator';
+export * from './lib/index';
