ecb2a88c1ad18a85a17472ad8db236ac790ce902
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-07-12 17:36:22 +0000

    removing build folder

--- example/lib/handler.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const lambda_1 = require("lambda");
-const story_service_1 = require("./api/story/story.service");
-module.exports = lambda_1.boostrap(story_service_1.StoryService, '');
-//# sourceMappingURL=handler.js.map
