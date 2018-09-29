f3dbd6f3faf51ccca9b746301f8e238ec6f2700e
Davide Cavaliere <cavaliere.davide@gmail.com>
2018-09-29 12:23:43 +0000

    chore: cleaning unused imports

--- src/lib/endpoint/endpoint.decorator.spec.ts
+++ src/lib/endpoint/endpoint.decorator.spec.ts
@@ -1,13 +1,10 @@
 // tslint:disable:no-expression-statement no-object-mutation
 import test from 'ava';
 import { Endpoint, EndpointOptions, getEndpointMetadata } from './endpoint.decorator';
-import { setNamespace } from '@microgamma/ts-debug/build/main/lib/log.decorator';
 
 const options: EndpointOptions = {
   name: 'endpoint-name'
 };
-
-setNamespace('lambda');
 
 @Endpoint(options)
 class TestClass {
